316ea891867a9a6f2a1ddf606a8fc230afbc854a
Gopabandhu Dash <gopabandhu@braintechnosys.com>
2022-02-12 01:05:01 +0530

    Initial Commit

---
+++ GraphQLDemoWithSwiftUI/GraphQLDemoWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLDemoWithSwiftUIApp.swift
+//  GraphQLDemoWithSwiftUI
+//
+//  Created by Brain Tech on 12/02/22.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLDemoWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
